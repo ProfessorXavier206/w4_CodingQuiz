af8dbd9c7f02d2497f4f26557f2f7c7e92a7e154
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-03-17 18:23:47 -0700

    3-17 commit made changes to HTML to give elements ID and Classes, and created display function and question to be randomly choosen.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ## 3-15
 
 - Created Start Button fuction as well as created the timer for quiz and placed the timer function into the start button function so that the timer start when the quiz button is pressed.
+
+## 3-17
+
+-created new vars for each element using class and IDs and created an array of question to be randomly generated regardless if button that was clicked was correct or not. Created display question functions for random creating question with choices started working on a function to check if the answer was correct or not and subtracts time if incorrect.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,62 @@
 console.log("Loading JS file");
 
 //VARIABLES
-var startBtn = document.getElementById("startBtn");
+var timerEl = document.getElementById("timerCount");
+var start = document.getElementById("startBtn");
+var guide = document.getElementsByClassName("guide");
+var qBox = document.getElementsByClassName("qBox");
+var finScreen = document.getElementsByClassName("finScreen");
 
-var timerElement = document.getElementById("timerCount");
+var questionsEl = document.getElementById("questions");
+var choiceEl = document.getElementById("choice");
 
 var time;
 var timerNum;
 
-//FUNCTIONS
+//list all Questions and Choices
+var quizQuestions = [
+  {
+    question: "What is HTML?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "What is Question 2 ?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "What is HTML?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "What is Question 2 ?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+];
+//var currentQIndex = 0; //starting index
+
+var questionIndex = Math.floor(Math.random() * quizQuestions.length);
+//console.log(questionIndex);
+// console.log(quizQuestions[1]);
+// console.log(quizQuestions[1].answer);
+// console.log(quizQuestions[1].question);
+// console.log(quizQuestions[1].choices[2]);
+
+//Start Quiz
 function startQuiz() {
-  console.log("Starting quiz Challenge");
-  //start the clock
-  //hide divs or show divs
   startTime();
+  displayQuestions();
 }
 
+//start the clock
 function startTime() {
   timerNum = 100;
   timeInterval = setInterval(function () {
     timerNum--;
-    timerElement.textContent = timerNum;
+    timerEl.textContent = timerNum;
 
     if (timerNum === 0) {
       clearInterval(timeInterval);
@@ -28,5 +64,36 @@ function startTime() {
   }, 1000);
 }
 
+function displayQuestions() {
+  questionsEl.textContent = quizQuestions[questionIndex].question;
+  //choiceEl.textContent = quizQuestions[1].choices;
+
+  //loop through all choices for a question
+  for (var i = 0; i < quizQuestions[questionIndex].choices.length; i++) {
+    //create a button for each option
+    var btn = document.createElement("button");
+    //setting the text for the button
+    btn.textContent = quizQuestions[questionIndex].choices[i];
+    //add event listiner
+    btn.addEventListener("click", checkChoice);
+    //append it to the div choice
+    choiceEl.append(btn);
+  }
+}
+
+function checkChoice() {
+  //console.log("Button clicked is ", this.textContent);
+  var answer = quizQuestions[questionIndex].answer;
+  if (this.textContent === answer) {
+    alert("Correct!!");
+  } else {
+    alert("you can do better !!");
+  }
+  //generate random index
+  //call displayQ
+}
+
+//hide divs or show divs
+
 //EVENT LISTENER
-startBtn.addEventListener("click", startQuiz);
+start.addEventListener("click", startQuiz);
